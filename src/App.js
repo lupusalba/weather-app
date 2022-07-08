78981be51cc9c0fd1256be8605a27720f87eb499
lupusalba <a145296@gmail.com>
2022-07-09 00:58:36 +0200

    all ok, small changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 function App() {
@@ -12,62 +12,71 @@ function App() {
   const [location, setLocation] = useState("")
   const [weather, setWeather] = useState({})
 
+  const [cities, setCities] = useState({})
+
   const search = e => {
     if (e.key === "Enter") {
       fetch(`${api.base}weather?q=${location}&units=metric&APPID=${api.key}`)
-      .then(response => response.json())
-      .then(result => {
-        setWeather(result)
-        setLocation("")
-        console.log(result)
-      })
+        .then(response => response.json())
+        .then(result => {
+          setWeather(result)
+          setLocation("")
+          console.log(result)
+        })
     }
   }
 
+
+
+
+
     const findDate = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    let day = days[d.getDay()]
-    let date = d.getDate()
-    let month = months[d.getMonth()]
-    let year = d.getFullYear()
+  let day = days[d.getDay()]
+  let date = d.getDate()
+  let month = months[d.getMonth()]
+  let year = d.getFullYear()
 
-    return `${day} ${date} ${month} ${year}`
-  }
+  return `${day} ${date} ${month} ${year}`
+}
 
 
-  return (
-    <div className="weatherApp">
+return (
+  <div className="weatherApp">
 
-      <div className="search">
-        <input
-          type="text"
-          className="searchBar"
-          placeholder="Search Location"
-          onChange={e => setLocation(e.target.value)}
-          value={location}
-          onKeyDown={search}
-        />
+    <h1 className="title">Weather app</h1>
+
+    <div className="search">
+      <input
+        type="text"
+        className="searchBar"
+        placeholder="Search Location"
+        onChange={e => setLocation(e.target.value)}
+        value={location}
+        onKeyDown={search}
+      />
+    </div>
+
+    {(typeof weather.main != "undefined") ? (
+      <div>
+
+        <div className="LocationData">
+          <div className="location">{weather.name}, {weather.sys.country}</div>
+          <div className="date">{findDate(new Date())}</div>
+        </div>
+
+        <div className="weatherData">
+          <div className="temperature">{Math.round(weather.main.temp)}°c</div>
+          <div className="weather">{weather.weather[0].main}</div>
+        </div>
       </div>
-        {(typeof weather.main != "undefined") ? (
-          <div>
+    ) : ("")}
 
-            <div className="LocationData">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
-              <div className="date">{findDate(new Date())}</div>
-            </div>
+  </div>
 
-            <div className="weatherData">
-              <div className="temperature">{Math.round(weather.main.temp)}°c</div>
-              <div className="weather">{weather.weather[0].main}</div>
-            </div>
-          </div>
-        ) : ("")}
-
-      </div>
-
-  )
+)
 }
 
 export default App;
