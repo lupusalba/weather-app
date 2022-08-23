a93415e56f180d43787bc1fee84b69d3206dd78a
lupusalba <a145296@gmail.com>
2022-08-23 17:20:54 +0200

    changed all functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
 import './App.css';
 import { useEffect, useState } from "react"
+import Axios from "axios"
 import searchIcon from "./Images/Icons/searchIcon.png"
 import RainImage from './Images/Backgrounds/rain1.jpg'
+import SnowImage from './Images/Backgrounds/snow.jpg'
+import CloudsImage from './Images/Backgrounds/clouds.jpg'
+import ClearSkyImage from './Images/Backgrounds/clear-sky.jpg'
+
+
 
 
 const oneData = {
@@ -49,6 +55,9 @@ const oneData = {
   "cod": 200
 }
 
+
+
+
 function App() {
   const api = {
     key: "5e23ba6621133f1210f0d95dca4df3a7",
@@ -58,148 +67,100 @@ function App() {
   const [location, setLocation] = useState("")
   const [weather, setWeather] = useState({})
 
-  const [cities, setCities] = useState({})
-
-  const search = e => {
-    if (e.key === "Enter") {
-      fetch(`${api.base}weather?q=${location}&units=metric&APPID=${api.key}`)
-        .then(response => response.json())
-        .then(result => {
-          setWeather(result)
-          setLocation("")
-          console.log(result)
-        })
+  const searchLocation = (e) => {
+    if (e.key === 'Enter') {
+      Axios.get(`${api.base}weather?q=${location}&units=metric&APPID=${api.key}`).then((res) => {
+        setWeather(res.data)
+        console.log("weather: " + weather.name)
+      })
+      setLocation('')
     }
   }
 
-
-
-
-
-  const findDate = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let day = days[d.getDay()]
-    let date = d.getDate()
-    let month = months[d.getMonth()]
-    let year = d.getFullYear()
-
-    return `${day} ${date} ${month} ${year}`
-  }
 
   const backgroundImage = {
     backgroundImage: `url(${RainImage})`
   }
 
-  const showCityData = (e) => {
-    let cityName = e.target.id
+return (
+  <div className="weatherApp" style={backgroundImage}>
+    <div id="wrapper">
+      <div id="mainData">
+        <div className="smallContainerWeather">
+          <div className="temperature">
+            {weather.main ? Math.round(weather.main.temp) : null}°c
+          </div>
 
-    fetch(`${api.base}weather?q=${cityName}&units=metric&APPID=${api.key}`)
-        .then(response => response.json())
-        .then(result => {
-          setWeather(result)
-          setLocation("")
-          console.log(result)
-        })
-  }
+          <div className="weatherDetails">
+            <div className="location">{weather ? weather.name : null}</div>
 
-
-  if (Object.keys(weather).length) {
-    fetch(`${api.base}weather?q=Berlin&units=metric&APPID=${api.key}`)
-    .then(response => response.json())
-    .then(result => {
-      setWeather(result)
-      setLocation("")
-      console.log(result)
-    })
-  }
-
-  console.log(weather)
-
-  return (
-    <div className="weatherApp" style={backgroundImage}>
-
-
-      <div id="wrapper">
-
-
-        <div id="mainData">
-
-          <div className="smallContainerWeather">
-            <div className="temperature">
-              {Math.round(oneData.main.temp)}°c
+            <div className="timeAndDate">
+              <div className="time">15:22</div>
+              {<div className="date">11.22.2000</div>}
             </div>
-
-            <div className="weatherDetails">
-
-              <div className="location">
-                {oneData.name}
-              </div>
-
-              <div className="timeAndDate">
-                <div className="time">15:22</div>
-                {<div className="date">11.22.2000</div>}
-              </div>
-
-
+          </div>
+          <div className="weatherIconWrapper">
+            <div className="weatherIcon">icon</div>
+            <div className="weatherName">
+              {weather.weather ? weather.weather[0].main : null}
             </div>
-            <div className="weatherIconWrapper">
-              <div className="weatherIcon">icon</div>
-              <div className="weatherName">{oneData.weather["main"]}</div>
-            </div>
-
           </div>
         </div>
-
-        <div className="aside">
-          <div className="asideCities">
-            <div className="search">
-              <input
-                type="text"
-                className="searchBar"
-                placeholder="Search Location"
-                onChange={e => setLocation(e.target.value)}
-                value={location}
-                onKeyDown={search}
-              />
-              <button onClick={search}>
-                <img id="searchIcon" src={searchIcon} alt="Search" />
-              </button>
-
-            </div>
-
-            <ul className="citiesList">
-              <li id="London" onClick={showCityData}>London</li>
-              <li id="Rome" onClick={showCityData}>Rome</li>
-              <li id="Berlin" onClick={showCityData}>Berlin</li>
-              <li id="New York" onClick={showCityData}>New York</li>
-            </ul>
-
-          </div>
-
-          <hr />
-          
-          <div className="asideWeatherData">
-            <h3>Weather Details</h3>
-
-            <ul className="weatherInfo">
-              <li><span>Humidity</span>{oneData.main.humidity}</li>
-              <li><span>Minimal Temperature</span>{oneData.main.temp_min}</li>
-              <li><span>Maximal Temperature</span>{oneData.main.temp_max}</li>
-              <li><span>Wind Speed</span>{oneData.wind.speed}</li>
-            </ul>
-
-          </div>
-        </div>
-
-
       </div>
 
+      <div className="aside">
+        <div className="asideCities">
+          <div className="search">
+            <input
+              type="text"
+              className="searchBar"
+              placeholder="Search Location"
+              onChange={(e) => setLocation(e.target.value)}
+              value={location}
+              onKeyDown={searchLocation}
+            />
 
+            <div onClick={searchLocation}>
+              <img id="searchIcon" src={searchIcon} alt="Search" />
+            </div>
+          </div>
+
+          {/* <ul className="citiesList">
+            <li id="London" onClick={showCityData}>London</li>
+            <li id="Rome" onClick={showCityData}>Rome</li>
+            <li id="Berlin" onClick={showCityData}>Berlin</li>
+            <li id="New York" onClick={showCityData}>New York</li>
+          </ul> */}
+        </div>
+
+        <hr />
+
+        <div className="asideWeatherData">
+          <h3>Weather Details</h3>
+
+          <ul className="weatherInfo">
+            <li>
+              <span>Humidity</span>
+              {weather.main ? weather.main.humidity : null}
+            </li>
+            <li>
+              <span>Minimal Temperature</span>
+              {weather.main ? weather.main.temp_min : null}
+            </li>
+            <li>
+              <span>Maximal Temperature</span>
+              {weather.main ? weather.main.temp_max : null}
+            </li>
+            <li>
+              <span>Wind Speed</span>
+              {weather.main ? weather.wind.speed : null}
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-
-  )
+  </div>
+);
 }
 
 export default App;
